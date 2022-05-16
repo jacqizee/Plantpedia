219e49bbdb33921caaacb8db03e0dac1bfaf7b95
Rob Green <robertma.green@gmail.com>
2022-05-16 15:04:26 +0100

    defined plant schema

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+import 'dotenv/config'
+
+const logger = (req, res, next) => {
+  console.log(`🚨 - Incoming request on ${req.method} - ${req.url}`)
+  next()
+}
+
+const startSever = async () => {
+  const app = express()
+
+  app.use(express.json())
+  app.use(logger)
+
+  app.get('/', (req, res, next) => {
+    return res.end('hi')
+  })
+
+  await mongoose.connect(process.env.MONGO_DB)
+  console.log("Connected to MongoDB!")
+  app.listen(process.env.PORT, () => console.log(`🚀 - Server listening on Port ${process.env.PORT}`))
+}
+
+startSever()
---
+++ models/plants.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import mongooseUniqueValidator from 'mongoose-unique-validator'
+
+
+const plantSchema = new mongoose.Schema({
+  name: { type: String, required: true, maxLength: 150 },
+  scientificName: { type: String, required: true, unique: true },
+  images: { type: String, required: true },
+  upkeep: {
+    watering: { type: String, enum: ['Daily', 'Weekly', 'Monthly'], required: true },
+    sunExposure: { type: String, enum: ['Full sun', 'Partial sun', 'Shade'], required: true },
+    soilType: { type: Array, enum: ['Loamy', 'Chalky', 'Peaty', 'Silty', 'Sandy', 'Clay'], required: true }
+  },
+  characteristics: {
+    flowerColor: { type: String, maxLength: 30 },
+    mood: { type: String, enum: ['Mysterious', 'Cheerful', 'Emo', 'Bright', 'Classy'] },
+    lifespan: { type: String, enum: ['Evergreen', 'Perennial', 'Biennial'] },
+    isIndoor: { type: Boolean },
+    matureSize: {
+      height: { type: Number },
+      width: { type: Number },
+    },
+  },
+})
