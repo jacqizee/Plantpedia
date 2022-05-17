1bc53da06121662c4d80357e5b972b78187e8abf
Jacqueline <jhjzhou@gmail.com>
2022-05-17 10:06:03 +0200

    added additional seed data for plants

--- models/plants.js
+++ models/plants.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose'
 import mongooseUniqueValidator from 'mongoose-unique-validator'
 
-
-
 // SUBDOCUMENT
 // Comment Schema
 const commentSchema = new mongoose.Schema({
@@ -10,23 +8,22 @@ const commentSchema = new mongoose.Schema({
   text: { type: String, required: true, maxlength: 350 },
   owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
 }, {
-  timestamps: true // setting timestamps to be true adds "createdAt" and "updatedAt" timestamps to our document
+  timestamps: true
 })
-
 
 const plantSchema = new mongoose.Schema({
   name: { type: String, required: true, maxLength: 150 },
   scientificName: { type: String, required: true, unique: true },
   images: { type: String, required: true },
   upkeep: {
-    watering: { type: String, enum: ['Daily', 'Weekly', 'Monthly'], required: true },
+    watering: { type: String, enum: ['Daily', 'Weekly', 'Bi-Weekly', 'Monthly'], required: true },
     sunExposure: { type: String, enum: ['Full sun', 'Partial sun', 'Shade'], required: true },
     soilType: { type: String, enum: ['Loamy', 'Chalky', 'Peaty', 'Silty', 'Sandy', 'Clay'], required: true },
   },
   characteristics: {
     flowerColor: { type: Array },
     mood: { type: String, enum: ['Mysterious', 'Cheerful', 'Emo', 'Bright', 'Classy'] },
-    lifespan: { type: String, enum: ['Evergreen', 'Perennial', 'Biennial'] },
+    lifespan: { type: String, enum: ['Perennial', 'Biennial', 'Annual'] },
     isIndoor: { type: Boolean },
     matureSize: {
       height: { type: Number },
