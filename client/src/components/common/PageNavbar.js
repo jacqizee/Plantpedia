673eef07e5c5d9686e1547ece271a05f452e2703
Rob Green <robertma.green@gmail.com>
2022-05-17 21:00:47 +0100

    started home page

--- client/src/components/common/PageNavbar.js
+++ client/src/components/common/PageNavbar.js
@@ -16,6 +16,7 @@ import AddIcon from '@mui/icons-material/Add'
 
 // Import logo image
 import logo from '../../images/logo.png'
+import philip from '../../images/philip.png'
 
 
 const pages = ['Add']
@@ -70,7 +71,7 @@ const PageNavbar  = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Philip Sopher" src={philip} />
               </IconButton>
             </Tooltip>
             <Menu
