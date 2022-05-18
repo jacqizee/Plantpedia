9069fa69e9fc77f29ff453c396cb16e00ad8f22d
Philip Sopher <phsophe@gmail.com>
2022-05-18 12:37:18 +0200

    userprofile started

---
+++ client/src/components/user/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const UserProfile = () => {
+  return (
+    <>
+      <h1>User Profile!</h1>
+      <Link to="/">Back to home</Link>
+    </>
+  )
+}
+
+export default UserProfile
