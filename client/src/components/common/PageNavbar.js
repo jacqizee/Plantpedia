20ab34303312ed40254604d8b4929c2e05c39ff2
Philip Sopher <phsophe@gmail.com>
2022-05-18 11:19:26 +0200

    nav bar now intelligent

--- client/src/components/common/PageNavbar.js
+++ client/src/components/common/PageNavbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
@@ -18,19 +19,33 @@ import AddIcon from '@mui/icons-material/Add'
 import logo from '../../images/logo.png'
 import philip from '../../images/philip.png'
 
+import { userIsAuthenticated } from '../../helpers/auth'
+
 
 const pages = ['Add']
 const pagesNoLogin = ['Login', 'Register']
 const settings = ['Profile', 'Logout']
 
 const PageNavbar  = () => {
+
+  // Navigate
+  const navigate = useNavigate()
+
   const [anchorElNav, setAnchorElNav] = React.useState(null)
   const [anchorElUser, setAnchorElUser] = React.useState(null)
 
-  const handleOpenNavMenu = (event) => {
+  const handleAdd = (event) => {
     console.log('plus clicked')
     setAnchorElNav(event.currentTarget)
   }
+
+  const navigateToSelected = (event) => {
+    const pageName = event.currentTarget.innerText.toLowerCase()
+    console.log('page to navigate', pageName)
+
+    navigate(`/${pageName}`)
+  }
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget)
   }
@@ -52,51 +67,69 @@ const PageNavbar  = () => {
           <Box as={Link} to="/" sx={{ width: 20, mr: 2 }}>
             <img src={logo} alt="Logo" />
           </Box>
-          
-          {/* Menu Items */}
-          <Box sx={{ flexGrow: 1, justifyContent: 'end', display: 'flex' }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <AddIcon />
-            </IconButton>
-          </Box>
 
-          {/* Profile image */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Philip Sopher" src={philip} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          {userIsAuthenticated() ? 
+            <>
+              {/* Menu Items */}
+              <Box sx={{ flexGrow: 1, justifyContent: 'end', display: 'flex' }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleAdd}
+                  color="inherit"
+                >
+                  <AddIcon />
+                </IconButton>
+              </Box>
+
+              {/* Profile image */}
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Philip Sopher" src={philip} />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </>
+            :
+            <>
+              <Box sx={{ flexGrow: 1, justifyContent: 'end', display: 'flex' }}>
+                {pagesNoLogin.map((page, index) => (
+                  <Button
+                    key={index}
+                    onClick={navigateToSelected}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+            </>
+          }
         </Toolbar>
       </Container>
     </AppBar>
--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -1,20 +1,34 @@
 // ? This function gets token from local storage
 export const getTokenFromLocalStorage = () => {
-  return 'get token'
+  return window.localStorage.getItem('plantpedia')
 }
 
 // ? this function takes the token, splits it up, returns the payload encoded using base64
 // This function is not a default export so will be destructured when importing
 export const getPayload = () => {
-  return 'getPayload'
+  const token = getTokenFromLocalStorage()
+  if (!token) return
+  const payload = token.split('.')[1]
+  console.log(JSON.parse(atob(payload)))
+  return JSON.parse(atob(payload))
 }
 
 // ? function that checks that user is authenticated
 export const userIsAuthenticated = () => {
-  return 'userIsAuthenticated'
+  const payload = getPayload()
+  if (!payload) return false
+  const currentTime = Math.floor(Date.now() / 1000)
+
+  const isAuthenticated = currentTime < payload.exp
+  console.log('is authenticated?', isAuthenticated)
+  return currentTime < payload.exp
 }
 
 // ? This function will check the user id from the payload matches the cheese user id
-export const userIsOwner = (something) => {
-  return something
+export const userIsOwner = (singlePlant) => {
+  
+  // get payload and check it has a value
+  const payload = getPayload()
+  if (!payload) return
+  return singlePlant.user._id === payload.sub
 }
