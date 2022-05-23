b9aeaf13933715341652c889d81145ec9280d1cf
Rob Green <robertma.green@gmail.com>
2022-05-23 16:14:49 +0100

    search bar shenaningans

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -41,6 +41,7 @@ const Home = () => {
   const [searchTerm, setSearchTerm] = useState([])
   const colorFilter = []
 
+
   const colors = [
     'Red',
     'Purple',
@@ -93,23 +94,21 @@ const Home = () => {
   // works for typing in a color and pressing enter
   useEffect(() => {
     const regexp = new RegExp(searchTerm, 'i')
-    console.log(searchTerm)
     const filteredArray = plants.filter(plant => regexp.test(plant.name))
     setSearchPlant(filteredArray)
   }, [searchTerm])
 
-  //! WIP
-
 
   const handleChipClick = (e, chip) => {
-
+    e.target.classList.toggle('styled')
     if (colors.includes(chip)) {
       colorFilter.includes(chip) ? colorFilter.splice(colorFilter.indexOf(chip), 1) : colorFilter.push(chip)
-      const filteredArray = (filteredPlants.length ? filteredPlants : plants).filter(plant => colorFilter.some(color => plant.flowerColor.includes(color)))
-      console.log(filteredArray)
       console.log(colorFilter)
+      const filteredArray = plants.filter(plant => colorFilter.some(color => plant.flowerColor.includes(color)))
+      // console.log(filteredArray)
+      // console.log(colorFilter)
       setFilteredPlants(filteredArray)
-    } 
+    }
   }
 
 
@@ -119,7 +118,7 @@ const Home = () => {
     <>
       {/* search bar */}
       <Container maxWidth='lg' >
-        <TextField fullWidth placeholder='Search by name...' onChange={handleInput} value={searchTerm} sx={{ pt: 3 }} />
+        <TextField fullWidth autoComplete='off' placeholder='Search by name...' onChange={handleInput} value={searchTerm} sx={{ pt: 3 }} />
         <Accordion disableGutters>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
@@ -130,8 +129,8 @@ const Home = () => {
             <Typography>Search by tags</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            {/* flower color */}
-            <Grid container textAlign='center'>
+            <Grid container spacing={1} textAlign='center'>
+              {/* flower color */}
               <Grid item xs={4}>
                 <Box>
                   <Typography pb={3}>Flower Color</Typography>
@@ -141,7 +140,6 @@ const Home = () => {
                         <Grid item xs={4} key={chip}>
                           <Box
                             as='span'
-                            className=''
                             onClick={(e) => handleChipClick(e, chip)}
                             sx={{
                               display: 'flex',
@@ -150,12 +148,12 @@ const Home = () => {
                               backgroundColor: '#ebebeb',
                               borderRadius: 10,
                               p: 1,
+                              color: [chip],
                               '&:hover': {
                                 cursor: 'pointer',
                                 backgroundColor: '#e0e0e0',
-                              },                              
+                              },
                             }}>
-                            <CircleIcon sx={{ color: [chip], width: '15px', mr: 1 }} />
                             {chip}
                           </Box>
                         </Grid>
@@ -169,32 +167,66 @@ const Home = () => {
               <Grid item xs={4}>
                 <Box>
                   <Typography pb={3}>Soil Type</Typography>
-                  {soilType.map((chip) => {
-                    return (
-                      <Chip
-                        onClick={(e) => handleChipClick(e, chip)}
-                        key={chip}
-                        label={chip}
-                        sx={{ width: '100px', mb: 1, mr: 1 }}
-                      />
-                    )
-                  })}
+                  <Grid container spacing={1}>
+                    {soilType.map((chip) => {
+                      return (
+                        <Grid item xs={4} key={chip}>
+                          <Box
+                            as='span'
+                            onClick={(e) => handleChipClick(e, chip)}
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              backgroundColor: '#ebebeb',
+                              borderRadius: 10,
+                              p: 1,
+                              color: [chip],
+                              '&:hover': {
+                                cursor: 'pointer',
+                                backgroundColor: '#e0e0e0',
+                              },
+                            }}>
+                            {chip}
+                          </Box>
+                        </Grid>
+                      )
+                    })}
+                  </Grid>
+
                 </Box>
               </Grid>
               {/* Mood*/}
               <Grid item xs={4}>
                 <Box>
                   <Typography pb={3}>Mood</Typography>
-                  {mood.map((chip) => {
-                    return (
-                      <Chip
-                        onClick={(e) => handleChipClick(e, chip)}
-                        key={chip}
-                        label={chip}
-                        sx={{ width: '100px', mb: 1, mr: 1 }}
-                      />
-                    )
-                  })}
+                  <Grid container spacing={1}>
+                    {mood.map((chip) => {
+                      return (
+                        <Grid item xs={4} key={chip}>
+                          <Box
+                            as='span'
+                            onClick={(e) => handleChipClick(e, chip)}
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              backgroundColor: '#ebebeb',
+                              borderRadius: 10,
+                              p: 1,
+                              color: [chip],
+                              '&:hover': {
+                                cursor: 'pointer',
+                                backgroundColor: '#e0e0e0',
+                              },
+                            }}>
+                            {chip}
+                          </Box>
+                        </Grid>
+                      )
+                    })}
+                  </Grid>
+
                 </Box>
               </Grid>
             </Grid>
