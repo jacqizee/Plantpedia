1717c70b55e5aff48957746268c0ecb8f27d2a11
Jacqueline <jhjzhou@gmail.com>
2022-05-25 10:10:18 +0200

    added gif to Not Found page

--- client/src/components/common/NotFound.js
+++ client/src/components/common/NotFound.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import sadPlant from '../../images/sad-plant.gif'
+
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
 const NotFound = () => {
   return (
-    <>
-      <h1>404 Not Found!</h1>
-      <Link to="/">Back to home</Link>
-    </>
+    <Container sx={{ height: '85vh', width: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Box component='img' src={sadPlant} alt='sad plant gif' sx={{ width: 150 }} />
+      <Typography variant='h4'>Page Not Found!</Typography>
+      <Typography as={Link} to="/">Back to Home</Typography>
+    </Container>
   )
 }
 
--- client/src/components/utilities/Spinner.js
+++ client/src/components/utilities/Spinner.js
@@ -6,8 +6,8 @@ import spinner from '../../images/plant-growing.gif'
 import Box from '@mui/material/Box'
 
 const Spinner = () => (
-  <Box sx={{ width: 150 }}>
-    <img src={spinner} alt="Spinner" className="spinner" />
+  <Box sx={{ width: 200 }}>
+    <Box component='img' src={spinner} alt="Spinner" className="spinner" />
   </Box>
 )
 
