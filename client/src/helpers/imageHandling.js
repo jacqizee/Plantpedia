aa103a061ce26c0b01fa153c1583f230cec56595
Rob Green <robertma.green@gmail.com>
2022-05-28 19:26:40 +0100

    margin on home page

--- client/src/helpers/imageHandling.js
+++ client/src/helpers/imageHandling.js
@@ -15,47 +15,49 @@ import ChatBubbleIcon from '@mui/icons-material/ChatBubble'
 export const getImageList = (imagesArray, xsColumns = 3, smColumns = 3, mdColumns = 3, marginY = 0, fromHome = false) => {
   return (
     <Container key={'1'} maxWidth='lg' sx={{ my: marginY }}>
-      <Masonry key={'2'} columns={{ xs: xsColumns, sm: smColumns, md: mdColumns }} spacing={1}>
-        {imagesArray.map(plant => {
-          return (
-            <ImageListItem key={plant._id} >
-              <Box as={Link} to={`/plants/${plant._id}`} >
-                <img
-                  src={`${plant.images}`}
-                  alt={plant.name}
-                  loading='lazy'
-                />
-              </Box>
-              {fromHome &&
-                <ImageListItemBar
-                  title={plant.name}
-                  sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
-                  actionIcon={
-                    <>
-                      <Box sx={{ display: 'flex', alignItems: 'center' }} >
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                          <FavoriteIcon sx={{ color: 'white' }}
-                            aria-label={`favorites for ${plant.name}`} />
-                          <Typography sx={{ display: 'inline', mr: 2, ml: 1, color: 'white' }}>
-                            {plant.favorites.length}
-                          </Typography>
+      <Box sx={{ marginLeft: '6px' }}>
+        <Masonry key={'2'} columns={{ xs: xsColumns, sm: smColumns, md: mdColumns }} spacing={1}>
+          {imagesArray.map(plant => {
+            return (
+              <ImageListItem key={plant._id} >
+                <Box as={Link} to={`/plants/${plant._id}`} >
+                  <img
+                    src={`${plant.images}`}
+                    alt={plant.name}
+                    loading='lazy'
+                  />
+                </Box>
+                {fromHome &&
+                  <ImageListItemBar
+                    title={plant.name}
+                    sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+                    actionIcon={
+                      <>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }} >
+                          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <FavoriteIcon sx={{ color: 'white' }}
+                              aria-label={`favorites for ${plant.name}`} />
+                            <Typography sx={{ display: 'inline', mr: 2, ml: 1, color: 'white' }}>
+                              {plant.favorites.length}
+                            </Typography>
+                          </Box>
+                          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <ChatBubbleIcon sx={{ color: 'white' }}
+                              aria-label={` comments for ${plant.name}`} />
+                            <Typography sx={{ display: 'inline', mr: 2, ml: 1, color: 'white' }}>
+                              {plant.comments.length}
+                            </Typography>
+                          </Box>
                         </Box>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                          <ChatBubbleIcon sx={{ color: 'white' }}
-                            aria-label={` comments for ${plant.name}`} />
-                          <Typography sx={{ display: 'inline', mr: 2, ml: 1, color: 'white' }}>
-                            {plant.comments.length}
-                          </Typography>
-                        </Box>
-                      </Box>
-                    </>
-                  }
-                />
-              }
-            </ImageListItem>
-          )
-        })}
-      </Masonry>
+                      </>
+                    }
+                  />
+                }
+              </ImageListItem>
+            )
+          })}
+        </Masonry>
+      </Box>
     </Container>
   )
 }
